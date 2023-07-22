cb52150ee4d244dd3202e11916e5a48074c848c2
Aldawai <boubakaridawai5@gmail.com>
2023-07-22 04:06:20 +0100

    Implement addTask, deleteTask, editTask, doneTask and deleteAllDoneTaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 import type { Component } from 'solid-js';
+import { createSignal, Show, For } from 'solid-js';
+import { Task } from './components/task/task'; 
+import { TaskForm } from './components/form/form';
+import { Modal } from './components/modal/modal';
+import { todos, removeDoneTasks } from './state/todo';
 
 import logo from './logo.svg';
-import styles from './App.module.css';
+import CSS from './global.module.css';
 
 const App: Component = () => {
+
+	const [modal, setModal] = createSignal(false);
+
+  function modalFunc() {
+    setModal(true);
+  }
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          Learn Solid
-      </header>
-    </div>
+  	<>
+    <main class={CSS.main}>
+    <h1 class={CSS.title}>Task list</h1>
+      <TaskForm></TaskForm>
+      <section class={CSS.tasksList}>
+      	<For each={todos}>
+	      	{task => {
+	      	  return (
+	      		<Task todo={task} modal={modalFunc}></Task>
+	      	  );
+	      	}}
+	      </For>
+      </section>
+      <button class={CSS.primaryBtn} onClick={removeDoneTasks}>Remove all done Tasks</button>
+    </main>
+    <Show when={modal()}>
+    	<Modal></Modal>
+    </Show>
+    </>
   );
 };
 
